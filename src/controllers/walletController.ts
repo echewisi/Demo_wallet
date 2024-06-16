ca5dc59feb3f1abb5bb16f422663124cd8007e03
echewisi <josephechewisi@gmail.com>
2024-06-16 16:34:30 +0100

    INIT: created a controller, `walletController`,  to handle wallet related routes and endpoints

---
+++ src/controllers/walletController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { fundAccountService, withdrawFundsService, transferFundsService } from "../services/walletService";
+
+
+
+
+export const fundAccountController = async (req: Request, res: Response) => {
+    const { userId, amount, password } = req.body;
+    try {
+      const result = await fundAccountService(userId, amount, password);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ message: `unable to fund account! ${error}` });
+    }
+  };
+  
+  export const transferFundsController = async (req: Request, res: Response) => {
+    const { userId, recipientId, amount, password } = req.body;
+    try {
+      const result = await transferFundsService(userId, recipientId, amount, password);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ message: `unable to transfer funds! ${error}` });
+    }
+  };
+  
+  export const withdrawFundsController = async (req: Request, res: Response) => {
+    const { userId, amount, password } = req.body;
+    try {
+      const result = await withdrawFundsService(userId, amount, password);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(400).json({ message: `unable to withdraw funds! ${error}` });
+    }
+  };
+  
