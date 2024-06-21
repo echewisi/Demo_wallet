7a37a7f9a373bcf2123afb8320c6100dc00a688c
echewisi <josephechewisi@gmail.com>
2024-06-21 04:24:44 +0100

    modified:  configured the .env correctly and added migrations

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,17 +1,17 @@
 import type { Knex } from "knex";
 import dotenv from 'dotenv'; 
 
-dotenv.config({path: './.env'});
+dotenv.config({path: '../.env'});
 
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "mysql2",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME 
+      host: process.env.DB_HOST || 'your localhost',
+      user: process.env.DB_USER || 'your user name',
+      password: process.env.DB_PASSWORD || 'your password',
+      database: process.env.DB_NAME || 'your database name' 
     },
     migrations: {
       directory: './migrations'
