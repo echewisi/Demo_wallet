84cdcef2319349e275ef4bc66cd7db84f96a549a
echewisi <josephechewisi@gmail.com>
2024-06-21 04:46:03 +0100

    new migration file added

--- src/migrations/20240621031203_modify_wallet_id.ts
+++ src/migrations/20240621031203_modify_wallet_id.ts
@@ -1,10 +1,13 @@
-import type { Knex } from "knex";
-
+import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("users", (table) => {
+    table.uuid("wallet_id").nullable().alter();
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("users", (table) => {
+    table.uuid("wallet_id").notNullable().alter();
+  });
 }
-
