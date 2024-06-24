be8438f540d45367b04fb1d6ab3e4d9221dede27
echewisi <josephechewisi@gmail.com>
2024-06-24 17:52:23 +0100

    corrected: missing migration file

---
+++ src/migrations/20240618125555_add_wallet_id_to_users.ts
@@ -0,0 +1,13 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table("users", (table) => {
+    table.uuid("wallet_id").references("wallet_id").inTable("wallets").notNullable().index();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table("users", (table) => {
+    table.dropColumn("wallet_id");
+  });
+}
